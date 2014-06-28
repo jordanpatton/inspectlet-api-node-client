0ea5a68af4f3669a0c3ca8b19214b77e46d3dc11
Jordan Patton <jordanpatton@gmail.com>
2014-06-28 16:48:48 -0400

    Added significant functionality, autoAuth mode, examples, and tested code.

---
+++ examples/getCaptures.autoAuth.js
@@ -0,0 +1,70 @@
+var inspectletapi = require('../lib/inspectletapi.js');
+var config = require('./config.json');
+
+var params = {
+  "searchparams": {
+    "daterange": "",
+    "daterangecustom": {
+      "start": "",
+      "end": ""
+    },
+    "seslength_start": "1",
+    "seslength_end": "unlimited",
+    "sestime_start": "0",
+    "sestime_end": "unlimited",
+    "startpage": {
+      "operator": "is",
+      "conditionval": ""
+    },
+    "exitpage": {
+      "operator": "is",
+      "conditionval": ""
+    },
+    "visitedpage": {
+      "operator": "is",
+      "conditionval": ""
+    },
+    "newold": {
+      "new": true,
+      "old": true
+    },
+    "notfinishedtyping": false,
+    "tags": {
+      "paneopen": "basic",
+      "tagslist": [{
+        "tag": ""
+      }],
+      "operatorslist": []
+    },
+    "funnel": [],
+    "funnelmode": "none",
+    "showstarredonly": false,
+    "ipaddr": "",
+    "country": "",
+    "devices": "all",
+    "trafficsource": {
+      "kind": "all",
+      "operator": "is",
+      "keyword": ""
+    }
+  },
+  "showcols": {
+    "ip": true,
+    "seslen": true,
+    "pagetitlestart": true,
+    "screensize": false,
+    "referrer": true,
+    "date": true,
+    "tags": true
+  },
+  "atpage": {
+    "resultspage": 1,
+    "resultsperpage": 10
+  }
+};
+
+var insp = new inspectletapi(config.username, config.password, true);
+
+insp.getCaptures(config.siteId, params, function (error, data) {
+  console.log(data);
+});
